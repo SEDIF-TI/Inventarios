5b58027bb92317403780503d43dedff9e681d623
iGarvet <oscarpandilo@gmail.com>
2026-06-18 13:33:39 -0600

    first commit

---
+++ back/src/main/java/com/inventarios/back/InventariosBackApplication.java
@@ -0,0 +1,11 @@
+package com.inventarios.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventariosBackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InventariosBackApplication.class, args);
+    }
+}
---
+++ back/src/test/java/com/inventarios/back/InventariosBackApplicationTests.java
@@ -0,0 +1,10 @@
+package com.inventarios.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventariosBackApplicationTests {
+    @Test
+    void contextLoads() {}
+}
